a47111ca8b7e6474ed0ac886de9cbbd7d5c991fd
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-06-04 11:35:35 -0300

    clone projects

--- runInstall.js
+++ runInstall.js
@@ -1,5 +1,56 @@
 const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
 const fetch = require('node-fetch');
+
+const mainProjects = {
+    "100554": {
+        "repo": "https://github.com/expansiva/mls-100554.git"
+    },
+    "102020": {
+        "repo": "https://github.com/expansiva/mls-102020.git"
+    },
+    "102025": {
+        "repo": "https://github.com/expansiva/mls-102025.git"
+    },
+    "102027": {
+        "repo": "https://github.com/expansiva/mls-102027.git"
+    },
+    "102029": {
+        "repo": "https://github.com/expansiva/mls-102029.git"
+    },
+    "102032": {
+        "repo": "https://github.com/expansiva/mls-102032.git"
+    },
+    "102033": {
+        "repo": "https://github.com/expansiva/mls-102033.git"
+    },
+    "102034": {
+        "repo": "https://github.com/expansiva/mls-102034.git"
+    },
+    "102036": {
+        "repo": "https://github.com/expansiva/mls-102036.git"
+    },
+    "102040": {
+        "repo": "https://github.com/expansiva/mls-102040.git"
+    }
+
+}
+
+async function cloneMainProjects() {
+    for (const [projectId, project] of Object.entries(mainProjects)) {
+        const destPath = path.join('.', `mls-${projectId}`);
+
+        if (fs.existsSync(destPath)) {
+            console.log(`mls-${projectId} already exists, skipping`);
+            continue;
+        }
+
+        console.log(`Cloning mls-${projectId} from ${project.repo}...`);
+        execSync(`git clone "${project.repo}" "${destPath}"`, { stdio: 'inherit' });
+        console.log(`mls-${projectId} cloned`);
+    }
+}
 
 async function downloadFile(url, path) {
     const response = await fetch(url);
@@ -14,10 +65,10 @@ async function downloadFile(url, path) {
         response.body.on('error', err => {
             reject(err);
         });
-        fileStream.on('finish', function() {
+        fileStream.on('finish', function () {
             resolve();
         });
-        fileStream.on('error', function(err) {
+        fileStream.on('error', function (err) {
             reject(err);
         });
     });
@@ -34,7 +85,7 @@ async function fetchJson(url) {
     }
 }
 
-async function runDownload(){
+async function runDownload() {
     try {
 
         fs.mkdirSync('./types', { recursive: true });
@@ -47,12 +98,14 @@ async function runDownload(){
 
         await downloadFile(urlMonaco, './types/monaco.d.ts');
         console.log('Get monaco definition');
-        
+
         await downloadFile(urlMls, './types/mls.d.ts');
         console.log('Get lib definition');
 
+        await cloneMainProjects();
+
     } catch (error) {
-        throw new Error('Erro dowloads files:'+ error.message)
+        throw new Error('Erro dowloads files:' + error.message)
     }
 }
 
